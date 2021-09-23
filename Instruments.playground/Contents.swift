7b5868034309cdc66bc9cdd33a89d0731ad14fec
Albert Pangestu <stamina.left@gmail.com>
2021-09-23 22:39:43 +0700

    New class named 'Music' that encapsulates 'notes' stored var & 'prepared' behavior.

--- Instruments.playground/Contents.swift
+++ Instruments.playground/Contents.swift
@@ -1,3 +1,15 @@
+class Music {
+    let notes: [String]
+    
+    init(notes: [String]) {
+        self.notes = notes
+    }
+    
+    func prepared() -> String {
+        return notes.joined(separator: " ")
+    }
+}
+
 // 1
 class Instrument {
     // 2
