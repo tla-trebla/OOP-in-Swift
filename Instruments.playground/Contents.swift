3846e0c33195961750b3087d39a23a61361e5244
Albert Pangestu <stamina.left@gmail.com>
2021-09-23 22:34:19 +0700

    Added new class named 'Instrument' with 'brand' var and init.

--- Instruments.playground/Contents.swift
+++ Instruments.playground/Contents.swift
@@ -1,3 +1,10 @@
-import UIKit
-
-var greeting = "Hello, playground"
+// 1
+class Instrument {
+    // 2
+    let brand: String
+    // 3
+    init(brand: String) {
+        // 4
+        self.brand = brand
+    }
+}
