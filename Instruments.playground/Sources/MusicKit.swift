d55d4db945d94666e97e47204e591cbe03094c7c
Albert Pangestu <stamina.left@gmail.com>
2021-09-25 00:20:54 +0700

    Added public stored property 'notes', public init for 'notes', and public behavior 'prepared()' to be able seen by other files.

--- Instruments.playground/Sources/MusicKit.swift
+++ Instruments.playground/Sources/MusicKit.swift
@@ -1,4 +1,13 @@
 // 1
 final public class Music {
+    // 2
+    public let notes: [String]
     
+    public init(notes: [String]) {
+        self.notes = notes
+    }
+    
+    public func prepared() -> String {
+        return notes.joined(separator: " ")
+    }
 }
