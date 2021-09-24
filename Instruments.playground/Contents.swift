14aeb392e9e3115df73655fd3775964b590b3d42
Albert Pangestu <stamina.left@gmail.com>
2021-09-25 00:17:01 +0700

    Deleted root class 'Music' and 'Instrument' at Contents.

--- Instruments.playground/Contents.swift
+++ Instruments.playground/Contents.swift
@@ -1,39 +1,3 @@
-class Music {
-    let notes: [String]
-    
-    init(notes: [String]) {
-        self.notes = notes
-    }
-    
-    func prepared() -> String {
-        return notes.joined(separator: " ")
-    }
-}
-
-// 1
-class Instrument {
-    // 2
-    let brand: String
-    // 3
-    init(brand: String) {
-        // 4
-        self.brand = brand
-    }
-    
-    func tune() -> String {
-        fatalError("Implement this method for \(brand)")
-    }
-    
-    func play(_ music: Music) -> String {
-        return music.prepared()
-    }
-    
-    func perform(_ music: Music) {
-        print(tune())
-        print(play(music))
-    }
-}
-
 // 1
 class Piano: Instrument {
     let hasPedals: Bool
